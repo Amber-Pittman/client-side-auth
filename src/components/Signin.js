5d2cf7f9643c36e4b598d7d5885219da2ab33cdc
Amber-Pittman <amber@amberpittman.com>
2019-11-07 16:46:10 -0500

    Changed from axios request to api request

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,12 +1,21 @@
 // #6 Create Signin Component
+
 // #11 Import useState
 import React, { useState } from "react"
-import api from "../utils/api"
+
+// #32 Instead of importing axios, we'll import api, axios can be removed
+import api from "../utils/api";
+// // #24 Import axios
+// import axios from "axios";
 
 function Signin(props) {
-	// #12 Set up some initial state
+	// #20 Set up error state for error component/message
+	//    It does not need preventDefault either
 	const [error, setError] = useState()
-	const [data, setData] = useState({ // remember this is an object and we don't have to construct a new object 
+	// #12 Set up some initial state
+	const [data, setData] = useState({ 
+		// remember this is an object and we don't have to construct 
+		//   a new object 
 		email: "",
 		password: "",
 	})
@@ -25,16 +34,48 @@ function Signin(props) {
 		})
 	}
 
+	// #22 We need a handle submit function for our button
 	const handleSubmit = (event) => {
+		// We do need preventDefault because it has default behavior of
+		//   refreshing the page. We don't want that
 		event.preventDefault()
 
-		// We are using are axios instance with predefined values,
-		// rather than just plain old axios
+
+		// // #23 We need to make an axios call
+		// 	// We are using are axios instance with predefined values,
+		// 	// rather than just plain old axios
+		// axios.post("http://localhost:8080/signin", data) // post SENDs data
+		// 	.then(result => {
+		// 		console.log(result.data)
+		// 		// #26 Save the toke in local storage
+		// 		localStorage.setItem("token", result.data.token)
+		// 		// now that we have that token stored in local storage, 
+		// 		//   we essentially have to send it as a header for any
+		// 		//   subsequent requests
+		// 		// we could do this by adding a 3rd param to post with a 
+		// 		//   headers value of Authorization and 
+		// 		//     localStorage.getItem("token")
+		// 		//   That's kind of verbose though. We don't want to redefine
+		// 		//     that every time we make an axios call.
+		// 		//   Instead, we'll create a new folder (utils) and a new file (api.js)
+
+		// 	})
+		// 	.catch(err => {
+		// 		setError(err.response.data.message)
+		// 	})
+		
+
+		//  #33 Instead of calling axios post, we'll use api as a function
+		//    in place of axios
 		api()
+			// Since base url is already set in api.js, just need "signin"
 			.post("/signin", data)
 			.then(result => {
 				// Store our new token in local storage so it persists
 				localStorage.setItem("token", result.data.token)
+				// Token is a cryptographically defined string that says we are who we say 
+				//   we are. It uses a timestamp as part of the generation.
+
 				// Redirect the user to their account page after logging in
 				props.history.push("/account")
 			})
@@ -45,7 +86,11 @@ function Signin(props) {
 	
 	return (
 		// #9 Return a form tag
+		// #25 Don't forget to add handleSubmit to the form!
 		<form onSubmit={handleSubmit}>
+			{/* #21 Inside the component, we can write a simple ternary that says
+			if error exists or something is in there that is undefined, show
+			a div with className of error, and display the error */}
 			{error && <div className="error">{error}</div>}
 
 			{/* #10 Create some input fields and a button for the form */}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,6 @@
+// #27 Create new file for authorization header
+
+// #28 Import axios
 import axios from "axios"
 
 // A helper function so we don't have to keep repeating this
@@ -5,11 +8,21 @@ export function getToken() {
 	return localStorage.getItem("token")
 }
 
+// #29 export a default function of the file
 // Create an axios helper with some predefined values
+// This is a really useful helper function
 export default function() {
 	return axios.create({
-		baseURL: "http://localhost:8080",
+		// `create` is creating an instance of an object with some 
+		//   pre-defined values on it
+
+		// #30 predefine the main url so you don't have to keep using the 
+		//   entire url over and over		
+		baseURL: "http://localhost:8080", // we'll never have to redefine it
+		
+		// #31 Predefine some headers to this axios instance
 		headers: {
+			// custom header
 			Authorization: getToken(),
 		},
 	})
