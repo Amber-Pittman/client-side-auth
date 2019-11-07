d8c4586d452d53286ee024bd4963f2f4e42ab02a
Amber-Pittman <amber@amberpittman.com>
2019-11-07 10:06:36 -0500

    Imported routes, made nav links, defined routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import "./App.css"
 import React from "react"
+
+// #3 Define some routes by importing from rrd
 import { Link, Route, withRouter } from "react-router-dom"
+
 import { getToken } from "../utils/api"
 import ProtectedRoute from "./ProtectedRoute"
 import Signin from "./Signin"
@@ -12,15 +15,17 @@ function App() {
 
 	return (
 		<div className="wrapper">
+			{/*  #4 Let's create a nav tag and create some links */}
 			<nav>
 				<Link to="/">Home</Link>
-
+				
 				{/* We can conditionally show links if logged in or not */}
 				{!signedIn && <Link to="/signin">Sign In</Link>}
 				{signedIn && <Link to="/account">My Account</Link>}
 				{signedIn && <Link to="/logout">Logout</Link>}
 			</nav>
 
+			{/* #5 Define some routes */}
 			<Route exact path="/signin" component={Signin} />
 			{/* These routes will require an auth token to be set, due to our handy HOC */}
 			<ProtectedRoute exact path="/account" component={Account} />
