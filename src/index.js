ea4b96f7609d476ff4c771097b78bcccc7ce3ae1
Amber-Pittman <amber@amberpittman.com>
2019-11-07 09:58:56 -0500

    Installed react router and wrapped App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
+// #1 install react router
 import { BrowserRouter } from "react-router-dom"
 import App from "./components/App"
 
 ReactDOM.render(
+	// #2 Wrap app in browser component
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
